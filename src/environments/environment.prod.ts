05624bdb81162198f2113e1cf41e2959e87cd82c
Andrey <andreyduque1987@gmail.com>
2022-10-05 09:24:01 -0500

    Se cambia ruta WebHook

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   logoUriVolquetas: 'https://www.urivolquetas.com/wp-content/uploads/2021/11/logo-urivolquetas-153x72.png',
-  bitrixUriVolquetas: 'https://urivolquetas.bitrix24.co/rest/33/o6bnjerf8d7qrcd2/',
+  bitrixUriVolquetas: 'https://urivolquetas.bitrix24.co/rest/33/4knwurmd58eouzv4/',
   appURL:'localhost:4200',
   app_ID: '',
   authURL: 'https://urivolquetas.bitrix24.com/oauth/authorize',
-  crm: 'https://urivolquetas.bitrix24.co/rest/33/o6bnjerf8d7qrcd2'
+  crm: 'https://urivolquetas.bitrix24.co/rest/33/4knwurmd58eouzv4'
 };
