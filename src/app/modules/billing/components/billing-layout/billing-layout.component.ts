c8f1e9fd7d9412d9f2cb6a4fcf24ff876d86c55b
Andrey <andreyduque1987@gmail.com>
2023-10-28 11:18:14 -0500

    Construcción parcial vista facturación

--- src/app/modules/billing/components/billing-layout/billing-layout.component.ts
+++ src/app/modules/billing/components/billing-layout/billing-layout.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-billing-layout',
   templateUrl: './billing-layout.component.html',
   styleUrls: ['./billing-layout.component.scss']
 })
-export class BillingLayoutComponent {
+export class BillingLayoutComponent implements OnInit {
+
+  filtroSeleccionado = '';
+
+  constructor(
+    private readonly formBuilder: FormBuilder,
+  ){}
+
+  ngOnInit(): void {
+    //this.seleccionarFiltro();
+  }
+
+  seleccionarFiltro(event?: any){
+    if (event) {
+      this.filtroSeleccionado = event.target.value;
+      console.log('Filtro seleccionado: ', this.filtroSeleccionado);
+    }
+  }
 
 }
+
